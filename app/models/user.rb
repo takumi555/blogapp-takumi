8c29be61f87fc2b9945ad2872877d950c4c65b65
takumi555 <htaaa838317@gmail.com>
2020-10-04 17:22:17 +0900

    画像のアップロード

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,12 +23,36 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  delegate :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
     articles.exists?(id: article.id)
   end
 
   def display_name
-    self.email.split('@').first
+    #ぼっち演算子
+    profile&.nickname || self.email.split('@').first
+  end
+
+  #def birthday
+  #  profile&.birthday
+  #end
+
+  #def gender
+    #profile&.gender
+  #end
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
   end
 end
